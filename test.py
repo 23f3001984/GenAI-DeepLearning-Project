18bfebef56ad50fff2a5e558246451ae3b214440
Nanda <23f3001984@ds.study.iitm.ac.in>
2026-07-01 21:17:37 +0530

    Adding Milestone 2

---
+++ test.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        def climb(n,memo={}):
+            if n==2:
+                return 2
+            if n==1:
+                return 1
+            # if n in memo:
+            #     return memo[n]
+            memo[n] = climb(n-1) + climb(n-2)
+            return memo[n]
+        return climb(n)
+
+c = Solution()
+print(c.climbStairs(44))
